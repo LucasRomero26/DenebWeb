f1e85bb9d881a00833b806732723af0b5d408928
LucasRomero26 <lucasromero@uninorte.edu.co>
2025-05-26 12:25:35 -0500

    rutas corregidas

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "/home/saqui/deneb-webapp/src/index.css";
+import "../src/index.css";
 import DenebWebApp from "./DenebWebApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
